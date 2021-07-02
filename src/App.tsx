445e274c49e1ef20a08d28640c1af2164cfbee56
Bhavesh Sharma <34792643+iBhavesh@users.noreply.github.com>
2021-07-02 08:21:08 +0000

    add formik

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,10 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import AuthPage from "./pages/AuthPage";
 import AuthOnly from "./components/AuthOnly";
-import ImageForm from "./ImageForm";
 
 function App() {
   return (
     <Switch>
-      <Route path="/upload">
-        <ImageForm />
-      </Route>
       <Route path="/" exact>
         <AuthOnly>
           <h1>Home</h1>
