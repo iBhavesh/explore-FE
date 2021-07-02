ac8ad7560581b5d5aa29057502ab0381a44316b0
Bhavesh Sharma <34792643+iBhavesh@users.noreply.github.com>
2021-07-02 12:07:17 +0000

    addd signup form

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,7 +1,58 @@
-import { useEffect } from "react";
 import { useHistory, useLocation } from "react-router-dom";
-import { useAppDispatch } from "../app/hooks";
-import { login } from "../features/auth/authSlice";
+import { Link as RouterLink } from "react-router-dom";
+import { Formik, Field, Form } from "formik";
+import * as Yup from "yup";
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
+import DateFnsUtils from "@date-io/date-fns";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { TextField } from "formik-material-ui";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { DatePicker } from "formik-material-ui-pickers";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+const initialValues = {
+  first_name: "",
+  last_name: "",
+  email: "",
+  date_of_birth: new Date(),
+  password: "",
+  confirm_password: "",
+};
+
+// type Values = typeof initialValues;
+
+const validationSchema = Yup.object({
+  first_name: Yup.string().required("Required"),
+  last_name: Yup.string().required("Required"),
+  email: Yup.string().email("Invalid email address").required("Required"),
+  date_of_birth: Yup.date().required("Required"),
+  password: Yup.string().required("Required"),
+  confirm_password: Yup.string().required("Required"),
+});
 
 const AuthPage = () => {
   const useQuery = () => {
@@ -10,20 +61,112 @@ const AuthPage = () => {
   const to = useQuery().get("to");
   const history = useHistory();
 
-  const dispatch = useAppDispatch();
-  useEffect(() => {
-    dispatch(login({ authToken: "auth", refreshToken: "refresh" }));
-  }, [dispatch]);
+  const classes = useStyles();
 
-  const submitHandler: React.FormEventHandler = (event) => {
-    event.preventDefault();
-    if (to) history.push(to);
-  };
   return (
-    <form onSubmit={submitHandler} method="post">
-      <h1>Auth Page</h1>
-      <button type="submit">Login</button>
-    </form>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              setSubmitting(false);
+              alert(JSON.stringify(values, null, 2));
+            }, 500);
+          }}
+        >
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <Form className={classes.form}>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <Field
+                    component={TextField}
+                    name="first_name"
+                    variant="outlined"
+                    fullWidth
+                    id="first_name"
+                    label="First Name"
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <Field
+                    component={TextField}
+                    variant="outlined"
+                    fullWidth
+                    id="last_name"
+                    label="Last Name"
+                    name="last_name"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Field
+                    component={TextField}
+                    variant="outlined"
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Field
+                    component={DatePicker}
+                    inputVariant="outlined"
+                    fullWidth
+                    id="date_of_birth"
+                    label="Date of birth"
+                    name="date_of_birth"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Field
+                    component={TextField}
+                    variant="outlined"
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Field
+                    component={TextField}
+                    variant="outlined"
+                    fullWidth
+                    name="confirm_password"
+                    label="Confirm Password"
+                    type="password"
+                    id="confirm_password"
+                  />
+                </Grid>
+              </Grid>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                Sign Up
+              </Button>
+              <Grid container justify="flex-end">
+                <Grid item>
+                  <Link component={RouterLink} to="/register" variant="body2">
+                    Don't have an account? Sign up
+                  </Link>
+                </Grid>
+              </Grid>
+            </Form>
+          </MuiPickersUtilsProvider>
+        </Formik>
+      </div>
+    </Container>
   );
 };
 
